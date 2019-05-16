229aca47532bdd96c05c7d98b96aaf7ada0040eb
suxinhaixp <suxinhaixp@163.com>
2019-05-16 18:13:09 +0800

    now

--- src/main/java/com/suxinhaixp/sit/dao/DataRepository.java
+++ src/main/java/com/suxinhaixp/sit/dao/DataRepository.java
@@ -1,6 +1,7 @@
 package com.suxinhaixp.sit.dao;
 
 //new
+//now
 import com.suxinhaixp.sit.entity.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
